2e6c5301960220b9975479d8639caede48546111
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-07-28 21:35:41 -0600

    feat: Se ha añadio la opccion de que el admin pueda contratar nuevo personal de trabajo y asignarle roles

--- src/data/staffRepository.ts
+++ src/data/staffRepository.ts
@@ -1,17 +1,19 @@
 import { pool } from './DB';
-import { Staff } from '../modules/staffModule';
 
 export class StaffRepository {
-  async getAll(): Promise<Staff[]> {
-    const res = await pool.query('SELECT * FROM staff ORDER BY id ASC');
-    return res.rows;
+  async getStaff() {
+    const query = 'SELECT id, full_name, role, email FROM staff ORDER BY id ASC;';
+    const result = await pool.query(query);
+    return result.rows;
   }
 
-  async create(fullName: string, role: string): Promise<Staff> {
-    const res = await pool.query(
-      'INSERT INTO staff (full_name, role) VALUES ($1, $2) RETURNING *',
-      [fullName, role]
-    );
-    return res.rows[0];
+  async createStaff(fullName: string, role: string, email: string) {
+    const query = `
+      INSERT INTO staff (full_name, role, email)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    const result = await pool.query(query, [fullName, role, email]);
+    return result.rows[0];
   }
 }
