7f01129c191d2e0784bec1b8fae632847d246cee
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-07-28 21:34:28 -0600

    feat: Se han añadido consultas para la creacion de nuevos espacios de trabajo para las areas coworking

--- src/data/spaceRepository.ts
+++ src/data/spaceRepository.ts
@@ -1,17 +1,19 @@
 import { pool } from './DB';
-import { Space } from '../modules/spaceModule';
 
 export class SpaceRepository {
-  async getAll(): Promise<Space[]> {
-    const res = await pool.query('SELECT * FROM spaces ORDER BY id ASC');
-    return res.rows;
+  async getSpaces() {
+    const query = 'SELECT id, name, building, capacity FROM spaces ORDER BY id ASC;';
+    const result = await pool.query(query);
+    return result.rows;
   }
 
-  async create(name: string, building: string, capacity: number): Promise<Space> {
-    const res = await pool.query(
-      'INSERT INTO spaces (name, building, capacity) VALUES ($1, $2, $3) RETURNING *',
-      [name, building, capacity]
-    );
-    return res.rows[0];
+  async createSpace(name: string, building: string, capacity: number) {
+    const query = `
+      INSERT INTO spaces (name, building, capacity)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    const result = await pool.query(query, [name, building, capacity]);
+    return result.rows[0];
   }
 }
