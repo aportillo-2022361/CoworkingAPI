0e9b56b2393196549c9b35b135157326a1269ddd
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-07-28 21:33:52 -0600

    feat: Se ha mejorado le implementacion de la membresia para los usuarios que no la tienen y una mejor asignacion de tarjetas

--- src/data/membershipsRepository.ts
+++ src/data/membershipsRepository.ts
@@ -1,17 +1,18 @@
 import { pool } from './DB';
-import { Membership } from '../modules/membershipsModule';
 
 export class MembershipsRepository {
-  async getAll(): Promise<Membership[]> {
-    const res = await pool.query('SELECT * FROM memberships ORDER BY id ASC');
-    return res.rows;
+  async getAll() {
+    const result = await pool.query('SELECT * FROM memberships ORDER BY id ASC;');
+    return result.rows;
   }
 
-  async create(name: string, price: number, durationDays: number): Promise<Membership> {
-    const res = await pool.query(
-      'INSERT INTO memberships (name, price, duration_days) VALUES ($1, $2, $3) RETURNING *',
-      [name, price, durationDays]
-    );
-    return res.rows[0];
+  async create(name: string, price: number, durationDays: number) {
+    const query = `
+      INSERT INTO memberships (name, price, duration_days)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    const result = await pool.query(query, [name, price, durationDays]);
+    return result.rows[0];
   }
 }
